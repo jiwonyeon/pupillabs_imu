b2b8a244cbee4eda91535207ded01d434ed49446
JiwonYeon <jyeon@stanford.edu>
2023-12-20 11:20:42 -0800

    update

--- camera_related_processes.py
+++ camera_related_processes.py
@@ -22,8 +22,8 @@ def contour_centers(contours, threshold):
 
 def filter_contours(contours):
     # get the centers of all contours
-    threshold = 0 
-    centers, contours = contour_centers(contours, threshold)
+    contour_mass_threshold = 0
+    centers, contours = contour_centers(contours, contour_mass_threshold)
 
     # merge contours that are close to each other
     merged_contours = []
@@ -39,12 +39,10 @@ def filter_contours(contours):
             merged_contours.append((contours[id]))
 
     # filter contours based on the mass size 
-    threshold = 100
-    centers, filtered_contours = contour_centers(merged_contours, threshold)
+    contour_mass_threshold = 100
+    centers, filtered_contours = contour_centers(merged_contours, contour_mass_threshold)
 
-    return filtered_contours
-
-
+    return centers, filtered_contours
 
 def get_target_marker(frame):
     # convert colors of the frame
@@ -61,23 +59,62 @@ def get_target_marker(frame):
 
     # find contours
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if not any(contours):
-        return None
+    if len(contours)==0:
+        return None, None
     
     # filter contours 
-    filtered_contours = filter_contours(contours)
+    centers, filtered_contours = filter_contours(contours)
     
-    cv2.drawContours(frame_masked, filtered_contours, -1, (0, 255, 0), 3)
+    # remain only the marker that's closest to the center
+    frame_center = np.array([frame.shape[1]/2, frame.shape[0]/2], dtype=int)
+    center_idx = np.argmin(np.linalg.norm(centers-frame_center, axis=1))
+    target_marker = filtered_contours[center_idx]
+    target_center = centers[center_idx]
 
-    
+    return target_center, target_marker
 
-def grab_markers(video):
+def grab_markers(video, cam_to_img, distortion):
     # iterate frames and detect target markers
+    center_markers = pd.DataFrame(columns=["frame", "center x", "center y", "marker"])
     for f in range(len(video)):
         frame = video[f].asnumpy()
-        markers = get_target_marker(frame)
+        undistorted_frame = cv2.undistort(frame, cam_to_img, distortion)
+        target_center, target_marker = get_target_marker(undistorted_frame)
 
-        if markers is not None:
-            pass
+        if target_center is not None:
+            center_markers.loc[len(center_markers)] = [int(f), target_center[0], target_center[1], target_marker]
+        else:
+            center_markers.loc[len(center_markers)] = [int(f), None, None, None]
 
-    # return markers
+    return center_markers
+
+def find_median_contour(contours):
+    xlims = [np.min(contours[:,0]), np.max(contours[:,0])]
+    ylims = [np.min(contours[:,1]), np.max(contours[:,1])]
+    width = xlims[1]-xlims[0]+1
+    height = ylims[1]-ylims[0]+1
+
+    # create a heatmap-like image
+    h_image = np.zeros((height, width), dtype=np.uint8)
+    for x, y in contours:
+        h_image[y-ylims[0], x-xlims[0]] += 1
+
+    # apply gaussian filter 
+    sigma = 2
+    from scipy.ndimage import gaussian_filter
+    h_image_smoothed = gaussian_filter(h_image.astype(float), sigma=sigma)
+
+    # threshold the smoothed image to get a binary mask
+    binary_mask = h_image_smoothed > 0.6
+
+    # apply morphological closing to enhance connected regions
+    kernel_size = 10
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    h_morpho = cv2.morphologyEx(binary_mask.astype(np.uint8), cv2.MORPH_CLOSE, kernel)
+
+    # find median contour
+    median_contour, _ = cv2.findContours(binary_mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    median_contour = np.squeeze(np.concatenate(median_contour))
+
+
+
--- detect_movement_from_video.py
+++ detect_movement_from_video.py
@@ -4,10 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from decord import VideoReader, cpu, gpu 
 import cv2
-from camera_related_processes import grab_markers
+from camera_related_processes import grab_markers, find_median_contour
 
+folder_list = glob.glob('/Users/jyeon/Documents/GitHub/pupillabs_imu/data/2023*')
 
-def main(folder):
+for f in range(len(folder_list)):
+    folder = folder_list[f]
+
     # get the video information 
     info_path = os.path.join(folder, 'info.json')
     config = None
@@ -16,7 +19,7 @@ def main(folder):
         key = list(info['template_data']['data'].keys())
         config = info['template_data']['data'][key[0]][0]
 
-    # find degrees the glasses moved
+    # pull out the session info
     params = config
     dist_wall = int(config[:params.find('cm')])
     params = params[params.find('cm')+3:]
@@ -24,27 +27,39 @@ def main(folder):
     direction = params[params.find('cm')+3:-2]
     session = int(config[-1])
 
+    # load camera intrinsics
+    cam_to_img = np.array(json.load(open(folder + '/scene_camera.json'))['camera_matrix'])
+    # load distortion coefficients
+    distortion = np.array(json.load(open(folder + '/scene_camera.json'))['distortion_coefficients'])
+
     # load video
     video_path = glob.glob(os.path.join(folder, '*.mp4'))[0]
     video = VideoReader(video_path, ctx=cpu(0))
 
-    # grab markers on the wall 
-    markers = grab_markers(video)
+    # grab markers closest to the center of frames
+    center_markers = grab_markers(video, cam_to_img, distortion)
+    center_markers = center_markers.dropna()
+    
+    # find the start and the end frames
+    center_pos = center_markers[['center x', 'center y']].to_numpy()
+    difference = np.linalg.norm(center_pos[1:] - center_pos[:-1], axis=1)
+    start_frame_id = center_markers['frame'].iloc[np.where(difference>=3)[0][0]]
+    end_frame_id = center_markers['frame'].iloc[np.where(difference>=3)[0][-1]]
+    
+    # find the median location of the initial and the final markers
+    start_pos = center_markers[center_markers['frame']<start_frame_id][['center x', 'center y']].median().to_numpy()
+    end_pos = center_markers[center_markers['frame']>end_frame_id][['center x', 'center y']].median().to_numpy()
+
+    # get the final marker's median contour to convert pixel to cm 
+    end_marker_contour = np.squeeze(np.concatenate(center_markers[center_markers['frame']>end_frame_id]['marker'].to_list()))
+    median_contours = find_median_contour(end_marker_contour)
+    
+
+    # calculate pixels to cm using a known marker size
+    ### this calculation is based on the size of the origin (end) marker
+    end_marker_size = (7, 6.5) # in cm, width and height
     
 
 
-
-
-    pass
-
-
-    
-
-
-if __name__ == '__main__':
-    folder_list = glob.glob('/Users/jyeon/Documents/GitHub/pupillabs_imu/data/2023*')
-    for f in range(len(folder_list)):
-        folder = folder_list[f]
-        main(folder)
 
     
